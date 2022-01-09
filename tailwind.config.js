df0a5bf567240c3062958e5b9f38e6a903748ecf
Mindaoudou <mindaoudou@Mindaoudous-2.local>
2022-01-09 13:26:57 +0100

    first  version of the website

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      Spectral: 'Spectral',
+      Helvetica: "Helvetica",
+      forever: ['Forever Brush Script', 'sans-serif'],
+                                                
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/images/BACKGROUND.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
